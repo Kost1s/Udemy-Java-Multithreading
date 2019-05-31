2ad13f444598bea007f382ea312bdb7a80739dfa
kantoniou <kantoniou@appliedfinancialtechnology.com>
2019-05-31 13:56:18 +0100

    multiple locks down

---
+++ src/main/java/com/kap/multiplelocks/App.java
@@ -0,0 +1,11 @@
+package com.kap.multiplelocks;
+
+/**
+ * @author Konstantinos Antoniou
+ */
+public class App {
+
+    public static void main(String[] args) {
+        new Worker().main();
+    }
+}
